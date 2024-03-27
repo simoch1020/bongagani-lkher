e646224b7781f2143a7b11da26f601afdee51826
simoch10 <36165780+simoch10@users.noreply.github.com>
2024-03-27 21:39:38 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["1143613800298332170", ""],
   botInvite: "",
   supportServer: "",
-  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'RTXX GG',
+  mongodbURL: "mongodb+srv://simoegs:simoch@cluster0.z3cjfv2.mongodb.net/?retryWrites=true&w=majority",
+  status: 'simo GG',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
@@ -18,7 +18,7 @@ module.exports = {
 
   voteManager: {
     status: false,
-    api_key: "",
+    api_key: "AIzaSyD4Ce3kYGG5LQhemWeIwIMadoJSBNObqB0",
     vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume"],
     vote_url: "",
   },
